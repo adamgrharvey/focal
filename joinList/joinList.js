c8522e5babafcaa57a8b45e8a95f441c2243a3b9
Adam <adamgrharvey@gmail.com>
2022-06-28 15:29:45 -0700

    adding files

---
+++ joinList/joinList.js
@@ -0,0 +1,25 @@
+/*
+ * Write a function that joins the contents of conceptList together
+ * into one String, concepts, with each list item separated from
+ * the previous by a comma.
+ *
+ * To implement this we'll create a joinList function which will take
+ * in any array of strings return a comma-separated string.
+ *
+ * Note: We can NOT use the built-in Array join function.
+ */
+
+// Write our function (we must define it too!) below
+// ...
+const joinList = function(arr) {
+  let out = "";
+  for (const i in arr) {
+    out += (arr[i] + ", ");
+  }
+  return out.slice(0,-2);
+};
+
+// Test / Driver Code below...
+const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
+const concepts = joinList(conceptList);
+console.log(`Today I learned about ${concepts}.`);
